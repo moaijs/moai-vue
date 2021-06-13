9a53cda17a160d4f15a3fca4ccec434369446c31
Tu Huynh <huynhminhtufu@gmail.com>
2021-06-13 19:42:38 +0700

    Initial Rollup local dev

---
+++ lib/core/src/components/button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>Hello {{ world }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "Button",
+  setup() {
+    const world = ref<String>("World");
+    return { world };
+  }
+});
+</script>
